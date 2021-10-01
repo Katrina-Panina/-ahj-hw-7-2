20e3d4bc543639b4079e9d258abe9cb2099adae8
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-10-01 17:18:15 +0300

    fix

--- src/js/ModalDelete.js
+++ src/js/ModalDelete.js
@@ -1,75 +1,75 @@
-import templateEngine from './TemplateEngine';
-import eventBus from './EventBus';
+import templateEngine from "./TemplateEngine";
+import eventBus from "./EventBus";
 
 export default class ModalDelete {
   constructor(container) {
     if (!(container instanceof HTMLElement)) {
-      throw new Error('Передайте HTML элемент');
+      throw new Error("Передайте HTML элемент");
     }
     this.container = container;
   }
 
   render() {
     return templateEngine.generate({
-      type: 'div',
+      type: "div",
       attr: {
-        class: ['modal__delete'],
+        class: ["modal__delete"],
       },
       content: [
         {
-          type: 'div',
+          type: "div",
           attr: {
-            class: ['modal__background'],
+            class: ["modal__background"],
           },
-          content: '',
+          content: "",
         },
         {
-          type: 'div',
+          type: "div",
           attr: {
-            class: ['modal__content'],
+            class: ["modal__content"],
           },
           content: [
             {
-              type: 'div',
+              type: "div",
               attr: {
-                class: ['modal__header'],
+                class: ["modal__header"],
               },
-              content: '',
+              content: "",
             },
             {
-              type: 'div',
+              type: "div",
               attr: {
-                class: ['modal__body'],
+                class: ["modal__body"],
               },
-              content: 'Are you sure you want to delete this ticket? This action is irreversible.',
+              content: "Are you sure you want to delete this ticket? This action is irreversible.",
             },
             {
-              type: 'div',
+              type: "div",
               attr: {
-                class: ['modal__footer'],
+                class: ["modal__footer"],
               },
               content: [
                 {
-                  type: 'div',
+                  type: "div",
                   attr: {
-                    class: ['modal__close'],
+                    class: ["modal__close"],
                   },
                   listener: {
-                    type: 'click',
+                    type: "click",
                     cb: (event) => this.close(event),
                   },
-                  content: 'Close',
+                  content: "Close",
                 },
                 {
-                  type: 'div',
+                  type: "div",
                   attr: {
-                    class: ['modal__ok'],
+                    class: ["modal__ok"],
                   },
                   listener: {
-                    type: 'click',
+                    type: "click",
                     cb: () => this.submit(),
                   },
-                  content: 'Ok',
+                  content: "Ok",
                 },
               ],
             },
@@ -84,21 +84,21 @@ export default class ModalDelete {
   }
 
   submit() {
-    eventBus.emit('deleteTicket');
-    this.modalElement.classList.remove('active');
+    eventBus.emit("deleteTicket");
+    this.modalElement.classList.remove("active");
   }
 
   close() {
-    this.modalElement.classList.remove('active');
+    this.modalElement.classList.remove("active");
   }
 
   showModal() {
-    const modalTitle = this.modalElement.querySelector('.modal__header');
-    modalTitle.textContent = 'Warning!';
-    this.modalElement.classList.add('active');
+    const modalTitle = this.modalElement.querySelector(".modal__header");
+    modalTitle.textContent = "Warning!";
+    this.modalElement.classList.add("active");
   }
 
   get modalElement() {
-    return this.container.querySelector('.modal__delete');
+    return this.container.querySelector(".modal__delete");
   }
 }
