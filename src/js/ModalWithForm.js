cc3ebc6185f08518bb557220192dd704bf5fba98
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-10-01 17:37:22 +0300

    fix

--- src/js/ModalWithForm.js
+++ src/js/ModalWithForm.js
@@ -1,102 +1,102 @@
-import templateEngine from './TemplateEngine';
-import eventBus from './EventBus';
+import templateEngine from "./TemplateEngine";
+import eventBus from "./EventBus";
 
 export default class ModalWithForm {
   constructor(container) {
     if (!(container instanceof HTMLElement)) {
-      throw new Error('Передайте HTML элемент');
+      throw new Error("Передайте HTML элемент");
     }
     this.container = container;
   }
 
   render() {
     return templateEngine.generate({
-      type: 'div',
+      type: "div",
       attr: {
-        class: ['modal__form'],
+        class: ["modal__form"],
       },
       content: [
         {
-          type: 'div',
+          type: "div",
           attr: {
-            class: ['modal__background'],
+            class: ["modal__background"],
           },
-          content: '',
+          content: "",
         },
         {
-          type: 'div',
+          type: "div",
           attr: {
-            class: ['modal__content'],
+            class: ["modal__content"],
           },
           content: [
             {
-              type: 'div',
+              type: "div",
               attr: {
-                class: ['modal__header'],
+                class: ["modal__header"],
               },
-              content: '',
+              content: "",
             },
             {
-              type: 'div',
+              type: "div",
               attr: {
-                class: ['modal__body'],
+                class: ["modal__body"],
               },
               content: {
-                type: 'form',
+                type: "form",
                 attr: {
-                  class: ['modal__form'],
-                  name: 'modal-form',
+                  class: ["modal__form"],
+                  name: "modal-form",
                 },
                 content: [
                   {
-                    type: 'div',
+                    type: "div",
                     attr: {
-                      class: ['form__group'],
+                      class: ["form__group"],
                     },
                     content: [
                       {
-                        type: 'label',
+                        type: "label",
                         attr: {
-                          class: ['form__label'],
-                          for: 'short-description',
+                          class: ["form__label"],
+                          for: "short-description",
                         },
-                        content: 'Short description',
+                        content: "Short description",
                       },
                       {
-                        type: 'input',
+                        type: "input",
                         attr: {
-                          class: ['form__input'],
-                          id: 'short-description',
-                          name: 'name',
-                          placeholder: 'Please enter a short description...',
+                          class: ["form__input"],
+                          id: "short-description",
+                          name: "name",
+                          placeholder: "Please enter a short description...",
                         },
-                        content: '',
+                        content: "",
                       },
                     ],
                   },
                   {
-                    type: 'div',
+                    type: "div",
                     attr: {
-                      class: ['form__group'],
+                      class: ["form__group"],
                     },
                     content: [
                       {
-                        type: 'label',
+                        type: "label",
                         attr: {
-                          class: ['form__label'],
-                          for: 'short-description',
+                          class: ["form__label"],
+                          for: "short-description",
                         },
-                        content: 'Detailed description',
+                        content: "Detailed description",
                       },
                       {
-                        type: 'textarea',
+                        type: "textarea",
                         attr: {
-                          class: ['form__textarea'],
-                          id: 'detailed-description',
-                          name: 'description',
-                          placeholder: 'Please enter a detailed description...',
+                          class: ["form__textarea"],
+                          id: "detailed-description",
+                          name: "description",
+                          placeholder: "Please enter a detailed description...",
                         },
-                        content: '',
+                        content: "",
                       },
                     ],
                   },
@@ -104,32 +104,32 @@ export default class ModalWithForm {
               },
             },
             {
-              type: 'div',
+              type: "div",
               attr: {
-                class: ['modal__footer'],
+                class: ["modal__footer"],
               },
               content: [
                 {
-                  type: 'div',
+                  type: "div",
                   attr: {
-                    class: ['modal__close'],
+                    class: ["modal__close"],
                   },
                   listener: {
-                    type: 'click',
+                    type: "click",
                     cb: (event) => this.close(event),
                   },
-                  content: 'Close',
+                  content: "Close",
                 },
                 {
-                  type: 'div',
+                  type: "div",
                   attr: {
-                    class: ['modal__ok'],
+                    class: ["modal__ok"],
                   },
                   listener: {
-                    type: 'click',
+                    type: "click",
                     cb: () => this.submit(),
                   },
-                  content: 'Ok',
+                  content: "Ok",
                 },
               ],
             },
@@ -149,44 +149,44 @@ export default class ModalWithForm {
       name: formElement.elements.name.value,
       description: formElement.elements.description.value,
     };
-    this.modalElement.querySelector('.modal__header').textContent = '';
+    this.modalElement.querySelector(".modal__header").textContent = "";
     this.close();
-    eventBus.emit('submit', data, this.type);
+    eventBus.emit("submit", data, this.type);
     this.type = null;
   }
 
   close() {
     this.clearForm();
-    this.modalElement.classList.remove('active');
+    this.modalElement.classList.remove("active");
   }
 
   showModal(type, data) {
-    const modalTitle = this.modalElement.querySelector('.modal__header');
+    const modalTitle = this.modalElement.querySelector(".modal__header");
     this.type = type;
-    if (type === 'create') {
-      modalTitle.textContent = 'Register new ticket';
-      this.modalElement.classList.add('active');
+    if (type === "create") {
+      modalTitle.textContent = "Register new ticket";
+      this.modalElement.classList.add("active");
     }
-    if (type === 'edit') {
+    if (type === "edit") {
       const { formElement } = this;
       formElement.elements.name.value = data.name;
-      formElement.elements.description.value = data.description || 'No information';
-      modalTitle.textContent = 'Edit ticket';
-      this.modalElement.classList.add('active');
+      formElement.elements.description.value = data.description || "No information";
+      modalTitle.textContent = "Edit ticket";
+      this.modalElement.classList.add("active");
     }
   }
 
   get modalElement() {
-    return this.container.querySelector('.modal__form');
+    return this.container.querySelector(".modal__form");
   }
 
   get formElement() {
-    return this.modalElement.querySelector('form');
+    return this.modalElement.querySelector("form");
   }
 
   clearForm() {
     const { formElement } = this;
-    formElement.elements.name.value = '';
-    formElement.elements.description.value = '';
+    formElement.elements.name.value = "";
+    formElement.elements.description.value = "";
   }
 }
