15a44b055a36c1f1aeb124ea38c1d378f720d1d9
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-10-01 17:57:15 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
-import HelpDesk from './HelpDesk';
+import HelpDesk from "./HelpDesk";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 const app = new HelpDesk(root);
 
